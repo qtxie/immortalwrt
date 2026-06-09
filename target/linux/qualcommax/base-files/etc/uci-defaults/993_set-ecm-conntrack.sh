90843dd8446e1cd79e7a1f2b887162a0862b6c92
Xie Qingtian <xqtxyz@gmail.com>
2026-06-09 16:48:01 +0800

    netfilter: enable chained conntrack events for NSS ECM

--- target/linux/qualcommax/base-files/etc/uci-defaults/993_set-ecm-conntrack.sh
+++ target/linux/qualcommax/base-files/etc/uci-defaults/993_set-ecm-conntrack.sh
@@ -4,6 +4,15 @@
 FILE="/etc/sysctl.d/qca-nss-ecm.conf"
 
 #修改最大连接数
-sed -i "s/nf_conntrack_max=.*/nf_conntrack_max=65535/g" $FILE
+sed -i "s/nf_conntrack_max=.*/nf_conntrack_max=65535/g" "$FILE"
+
+# Enable conntrack event extension allocation for ctnetlink subscribers.
+if grep -q "^net.netfilter.nf_conntrack_events=" "$FILE"; then
+	sed -i "s/^net.netfilter.nf_conntrack_events=.*/net.netfilter.nf_conntrack_events=1/g" "$FILE"
+else
+	echo "net.netfilter.nf_conntrack_events=1" >> "$FILE"
+fi
+
+sysctl -w net.netfilter.nf_conntrack_events=1 >/dev/null 2>&1 || true
 
 exit 0
